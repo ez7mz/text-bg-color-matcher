ab81de03cbc4b3fbb80a19661d9e0cd13d1051c1
mesrarhamza48@gmail.com <mesrarhamza48@gmail.com>
2024-01-05 23:42:41 +0100

    link frontend with backend

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,21 @@
 from typing import Union
+from pydantic import BaseModel
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+class Message(BaseModel):
+    msg: str
+
+# Set up CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+)
 
 @app.get("/")
 def read_root():
@@ -10,3 +24,7 @@ def read_root():
 @app.get("/api/v1/")
 async def read_root():
     return {"message": "Hello World from the API"}
+
+@app.post("/api/v1/predict/")
+async def read_root(message: Message):
+    return {"message": f"You sent: {message.msg}"}
